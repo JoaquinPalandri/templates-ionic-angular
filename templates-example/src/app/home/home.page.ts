07d59445294c2e77a123414c1a3fc18d92c72a4d
joaquin <jpalandri@kozaca.com.ar>
2025-04-04 16:16:36 -0300

    Se crea el servicio auth-boolean.service para manejar el logueo mediante un servicio, Se utiliza BehaviorSubject para emitir que hubo un cambio

--- templates-example/src/app/home/home.page.ts
+++ templates-example/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ScreenSizeService } from '../services/screen-size.service';
+import { AuthBooleanService } from '../services/auth-boolean.service';
 
 @Component({
   selector: 'app-home',
@@ -8,19 +9,36 @@ import { ScreenSizeService } from '../services/screen-size.service';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
-export class HomePage {
-
+export class HomePage implements OnInit, OnDestroy {
   isMobile: boolean = false;
-  private subscription!: Subscription;
+  isLoggedIn: boolean = false;
+  private subscriptions = new Subscription();
 
-  constructor(private screenSizeService: ScreenSizeService) {}
+  constructor(
+    private screenSizeService: ScreenSizeService,
+    private authService: AuthBooleanService
+  ) {}
 
   ngOnInit() {
-    this.subscription = this.screenSizeService.getIsMobileObservable()
-      .subscribe(isMobile => this.isMobile = isMobile);
+    // Suscribirse al tamaño de pantalla
+    this.subscriptions.add(
+      this.screenSizeService.getIsMobileObservable()
+        .subscribe(isMobile => this.isMobile = isMobile)
+    );
+
+    // Suscribirse al estado de autenticación
+    this.subscriptions.add(
+      this.authService.isUserAuthenticated()
+        .subscribe(isLoggedIn => this.isLoggedIn = isLoggedIn)
+    );
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.subscriptions.unsubscribe();
+  }
+
+  logServiceAction() {
+    this.isLoggedIn ? this.authService.logout() : this.authService.login();
+    console.log('Service action executed:', this.isLoggedIn);
   }
 }
