b1bd6c6520b40739a43a4612972635bd66446d46
joaquin <jpalandri@kozaca.com.ar>
2025-04-04 15:37:59 -0300

    Se crea un template de form con distintos inputs para poder reutilizarlo

--- templates-example/src/app/components/forms/form-template/form-template.component.ts
+++ templates-example/src/app/components/forms/form-template/form-template.component.ts
@@ -1,14 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-template',
   templateUrl: './form-template.component.html',
   styleUrls: ['./form-template.component.scss'],
 })
-export class FormTemplateComponent  implements OnInit {
+export class FormTemplateComponent implements OnInit {
+  usuario: FormGroup;
+  showPassword = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.usuario = this.fb.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefono: ['', Validators.required],
+      fechaNacimiento: [''],
+      genero: [''],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: [''],
+      terminos: [false, Validators.requiredTrue],
+    }, { validator: this.passwordMatchValidator });
+  }
 
   ngOnInit() {}
 
+  guardarDatos() {
+    if (this.usuario.valid) {
+      console.log('Formulario válido, datos:', this.usuario.value);
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  private passwordMatchValidator(form: FormGroup) {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { notMatching: true };
+  }
 }
--- templates-example/src/app/components/forms/forms.module.ts
+++ templates-example/src/app/components/forms/forms.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormTemplateComponent } from './form-template/form-template.component';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [FormTemplateComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  exports: [FormTemplateComponent],
+  exports: [FormTemplateComponent,],
 })
 export class FormulariosModule { }
